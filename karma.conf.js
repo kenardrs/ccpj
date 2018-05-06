6bfd2f56d85c23eddeda7ab8023917770989cd53
kenardrs <kenardrs@gmail.com>
2018-05-06 16:59:23 -0300

    preparando o modelo com karma 1

---
+++ karma.conf.js
@@ -0,0 +1,25 @@
+// karma.conf.js
+module.exports = function(config){
+    config.set(
+        {
+            basePath:'./src',
+
+            files: [
+               '../node_modules/angular/angular.js', 
+               '../node_modules/angular-mocks/angular-mocks.js',
+            //    '**/*.module.js',
+            //    '*!(.module|.spec).js',
+               '**/*.spec.js'
+            ],
+            frameworks: ['jasmine'],
+
+            browsers: ['Chrome'],
+
+            plugins: [
+              'karma-chrome-launcher',
+              'karma-jasmine'
+            ]
+        
+        }
+    )
+}
---
+++ src/app/app.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('teste1', function() {
+
+    it('deve realizar o teste inicial', function(){
+        expect(1).toBe(1);
+    }) 
+
+})
